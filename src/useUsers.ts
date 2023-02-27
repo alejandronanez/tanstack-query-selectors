1fc3a18eb22a8e9dd4d86d05e0e50bf4e307ca25
Alejandro Nanez <git@alejandro.dev>
2023-02-26 22:42:19 -0500

    wait async fn

--- src/useUsers.ts
+++ src/useUsers.ts
@@ -1,5 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 
+const wait = async () => new Promise((resolve) => setTimeout(resolve, 500));
+
 export type UserShape = {
   id: number;
   name: string;
@@ -25,6 +27,7 @@ export type UserShape = {
 };
 
 async function fetchUsers(): Promise<UserShape> {
+  await wait();
   const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
 
   return await response.json();
